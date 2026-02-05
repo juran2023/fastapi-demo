e278bedaa833c79d3f1e5fb414ef18af6f59a109
jufj <juranfuture@gmail.com>
2026-02-05 11:23:55 +0800

    strings and f-strings

---
+++ src/playground/03_string.py
@@ -0,0 +1,26 @@
+import re
+
+name = "Alice"
+age = 20
+print(f"{name} is {age}")
+print(f"{name=}, {age=}")
+
+s = " hello \n"
+print(s.strip())
+print("a,b,c".split(","))
+print("".join(["a", "b", "c"]))
+
+
+def to_slug(s) -> str:
+    try:
+        newS = re.sub("(?<=\w)\s+(?=\w)", "-", s.lower())
+        # newS = re.sub("\s+", "-", s.lower())
+        return re.sub("[^\w-]", "", newS)
+
+    except TypeError:
+        return None
+    finally:
+        print(f"raw s = {s}")
+
+
+print(to_slug("Hello                          World !"))
