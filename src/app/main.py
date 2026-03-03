c70454a027065bdabdf37a37786b15a5abf8080a
jufj <juranfuture@gmail.com>
2026-03-03 17:55:12 +0800

    query parameter

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,15 @@ from enum import Enum
 
 app = FastAPI()
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+fake_item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
@@ -14,19 +19,33 @@ async def root():
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
+async def read_item(item_id: int, q: str = None, short: bool = False):
+    if q:
+        return {"item_id": item_id, "q": q}
+    if not short:
+        return {
+            "item_id": item_id,
+            "description": "This is an amazing item that has a long description",
+        }
     return {"item_id": item_id}
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    
+
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
-    
+
     return {"model_name": model_name, "message": "Have some residuals"}
+
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/items/")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_item_db[skip : skip + limit]
